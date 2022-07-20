8368a6f70f471a689c3da52f698f89f2290974d4
@therightchoyce <chris@therightchoyce.com>
2022-07-20 14:51:24 -0400

    Update layout for landing page

--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { ReactElement, useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
-import Landing from "../layout/landing";
+import Layout from "../layout/GalleryLayout";
 import type { NextPageWithLayout } from "./_app";
 
 const HomePage: NextPageWithLayout = () => {
@@ -21,72 +21,64 @@ const HomePage: NextPageWithLayout = () => {
   }, [address, isReconnecting, isDisconnected]);
 
   return (
-    <>
-      <div className="px-2 mt-[5vh] mb-[5vh] text-center">
-        <div className="w-[80vw] md:w-[70vw] mx-auto">
-          <Link href="/">
-            <a>
-              <Image
-                src="/altered-earth.svg"
-                width={1500}
-                height={155}
-                alt="Altered Earth"
-              />
-            </a>
-          </Link>
-          <h2 className="text-lg md:text-xl lg:text-3xl mt-[2vh]">
-            Exploring the earth in unseen ways
-          </h2>
+    <div className="flex flex-col lg:flex-row">
+      {/* Left nav bar -- none */}
+
+      {/* content */}
+      <div className="w-[100vw] lg:pl-8 lg:w-[90vw] lg:ml-[10vw]">
+        <div className="px-2 mt-[5vh] mb-[5vh] text-center lg:text-left">
+          <div className="w-[100vw] md:w-[70vw] mx-auto lg:mx-0">
+            <h1 className="text-5xl leading-relaxed lg:text-7xl lg:mb-8 custom-major-mono">
+              Altered eArth
+            </h1>
+            <h2 className="text-lg md:text-xl lg:text-3xl">
+              Exploring the earth in unseen ways
+            </h2>
+          </div>
+        </div>
+        <div
+          className={`${imageClass} mx-auto lg:mx-0 text-center lg:text-left`}
+        >
+          <div className="mb-16">
+            <Link href="/the-hydra">
+              <a>
+                <h2 className="text-2xl lg:text-4xl custom-major-mono mb-4">
+                  the HydrA
+                </h2>
+              </a>
+            </Link>
+            <Link href="/the-hydra">
+              <a>
+                <Image
+                  src="/the-hydra/hydra-hero.png"
+                  alt="The Hydra Collection"
+                  layout="intrinsic"
+                  width={1443}
+                  height={658}
+                />
+              </a>
+            </Link>
+          </div>
+
+          <div className="mb-16">
+            <h2 className="text-2xl lg:text-4xl custom-major-mono mb-4">
+              crystAliZed <small>(coming soon)</small>
+            </h2>
+            <Image
+              src="/crystalixed/crystalixed-hero.png"
+              alt="CRYSTALIXED"
+              width={1443}
+              height={658}
+            />
+          </div>
         </div>
       </div>
-
-      <div className={`${imageClass} mx-auto`}>
-        <div className="relative mb-16">
-          <Link href="/the-hydra">
-            <a>
-              <Image
-                src="/the-hydra/hydra-hero.png"
-                alt="The Hydra Collection"
-                layout="intrinsic"
-                width={1443}
-                height={658}
-              />
-            </a>
-          </Link>
-
-          <h2 className="invisible md:visible text-4xl uppercase absolute right-16 bottom-16 backdrop-blur-sm hover:underline">
-            <Link href="/the-hydra">
-              <a>The Hydra</a>
-            </Link>
-          </h2>
-          <h2 className="visible md:invisible text-xl uppercase hover:underline text-center">
-            <Link href="/the-hydra">
-              <a>The Hydra</a>
-            </Link>
-          </h2>
-        </div>
-
-        <div className="relative">
-          <Image
-            src="/crystalixed/crystalixed-hero.png"
-            alt="CRYSTALIXED"
-            width={1443}
-            height={658}
-          />
-          <h2 className="invisible md:visible text-4xl uppercase absolute right-16 bottom-16 backdrop-blur-sm">
-            CRYSTALIXED - Coming soon
-          </h2>
-          <h2 className="visible md:invisible text-xl uppercase text-center">
-            CRYSTALIXED - Comming soon
-          </h2>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
 HomePage.getLayout = function getLayout(page: ReactElement) {
-  return <Landing>{page}</Landing>;
+  return <Layout>{page}</Layout>;
 };
 
 export default HomePage;
