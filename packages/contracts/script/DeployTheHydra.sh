d14e3d2c50c0be2248db119a5d32ef9609f273d7
@therightchoyce <chris@therightchoyce.com>
2022-07-27 11:08:40 -0400

    Add shell script to automatically update the deploy addresses in the dapp package

--- packages/contracts/script/DeployTheHydra.sh
+++ packages/contracts/script/DeployTheHydra.sh
@@ -10,8 +10,13 @@ if [ -z "$CHAIN_ID" ]; then
   exit 1
 fi
 
+CONTRACT_NAME="TheHydra"
+DEPLOY_OUTPUT="deploys/$CHAIN_NAME/$CONTRACT_NAME.json"
+
 forge script script/DeployTheHydra.s.sol:DeployTheHydra -vvvv --ffi --chain-id $CHAIN_ID \
     --rpc-url $RPC_URL \
-    --private-key $DEPLOYER_PRIVATE_KEY \
-    --broadcast \
-    --verify --etherscan-api-key $ETHERSCAN_API_KEY
+    --private-key $DEPLOYER_PRIVATE_KEY
+    # --broadcast \
+    # --verify --etherscan-api-key $ETHERSCAN_API_KEY
+
+jq '{deployedTo: .transactions[0].contractAddress, deployer: .transactions[0].tx.from, transactionHash: .transactions[0].hash}' ./broadcast/DeployTheHydra.s.sol/$CHAIN_ID/run-latest.json > $DEPLOY_OUTPUT
