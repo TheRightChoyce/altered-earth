58ae3d1207536774cbd4a10e7abb6ea7ea5017d6
@therightchoyce <chris@therightchoyce.com>
2022-07-27 18:59:00 -0400

    Setup basic subgraph

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,11 +1,11 @@
-import { ExampleNFT, Transfer } from "../generated/ExampleNFT/ExampleNFT";
 import { Token } from "../generated/schema";
+import { TheHydra, Transfer } from "../generated/TheHydra/TheHydra";
 
 export function handleTransfer(event: Transfer): void {
-  const contract = ExampleNFT.bind(event.address);
+  const contract = TheHydra.bind(event.address);
 
-  const token = new Token(event.params.tokenId.toString());
+  const token = new Token(event.params.id.toString());
   token.owner = event.params.to;
-  token.tokenURI = contract.tokenURI(event.params.tokenId);
+  token.tokenURI = contract.tokenURI(event.params.id);
   token.save();
 }
