08cebd0e5932e51b899d54513448485ad5f690c4
@therightchoyce <chris@therightchoyce.com>
2022-08-13 19:41:26 -0400

    Update OS and LR buttions to properly link per networkwq

--- packages/app/src/LooksRareButton.tsx
+++ packages/app/src/LooksRareButton.tsx
@@ -1,10 +1,18 @@
 import { theHydraContract } from "./contracts";
 
 export const LooksRareButton = ({ tokenId }: { tokenId: number }) => {
+  const getLRUri = () => {
+    if (process.env.NEXT_PUBLIC_CHAIN_ID === "1") {
+      return "https://looksrare.org";
+    } else {
+      return `https://${process.env.NEXT_PUBLIC_CHAIN_NAME?.toLowerCase()}.looksrare.org`;
+    }
+  };
+
   return (
     <a
       key={tokenId}
-      href={`https://goerli.looksrare.org/collections/${theHydraContract.address}/${tokenId}`}
+      href={`${getLRUri()}/collections/${theHydraContract.address}/${tokenId}`}
       target="_blank"
       rel="noopener noreferrer"
       className="text-gray-200 border-2 border-slate-800 hover:border-sky-400 py-2 px-2 leading-none bg-slate-600 block w-full text-center"
--- packages/app/src/OpenSeaButton.tsx
+++ packages/app/src/OpenSeaButton.tsx
@@ -1,10 +1,23 @@
 import { theHydraContract } from "./contracts";
 
 export const OpenSeaButton = ({ tokenId }: { tokenId: number }) => {
+  const openSeaUrl = () => {
+    if (process.env.NEXT_PUBLIC_CHAIN_ID === "1") {
+      return "https://opensea.io";
+    } else {
+      return "https://testnets.opensea.io";
+    }
+  };
+  const openSeaCollectionUri = () => {
+    return theHydraContract.address;
+  };
+  const networkName = () => {
+    return process.env.NEXT_PUBLIC_CHAIN_NAME?.toLowerCase();
+  };
   return (
     <a
       key={tokenId}
-      href={`https://testnets.opensea.io/${theHydraContract.address}/${tokenId}`}
+      href={`${openSeaUrl()}/assets/${networkName()}/${openSeaCollectionUri()}/${tokenId}`}
       target="_blank"
       rel="noopener noreferrer"
       className="text-gray-200 border-2 border-slate-800 hover:border-sky-400 py-2 px-2 leading-none bg-slate-600 block w-full text-center"
