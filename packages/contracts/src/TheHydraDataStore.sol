82e36f64c75f214c8f0f4dc953101736ca42c5f7
@therightchoyce <chris@therightchoyce.com>
2022-08-13 23:32:36 -0400

    Update DataStore to have a min and max length.. for some reason my xqstgfx files differ by a few bytes from file to file

--- packages/contracts/src/TheHydraDataStore.sol
+++ packages/contracts/src/TheHydraDataStore.sol
@@ -5,21 +5,21 @@ import "./interfaces/ITheHydraDataStore.sol";
 import "solmate/auth/Owned.sol";
 import "solmate/utils/SSTORE2.sol";
 
-/// @title TheHydra is the genesis collection of the Altered Earth NFT series. 
+/// @title TheHydra is the genesis collection of the Altered Earth NFT series.
 /// @author therightchoyce.eth
 /// @notice This contract acts as an on-chain data store for various pieces of the artwork
 /// @dev Uses SSTORE2 to get/set raw data on-chain
 contract TheHydraDataStore is ITheHydraDataStore, Owned {
-
     // --------------------------------------------------------
     // ~~ Internal storage  ~~
     // --------------------------------------------------------
 
     /// @dev Uri for off chain storage.. i.e. an IPFS link -- This is private since we need to expose the function in the interface in order to allow for cross-contract interaction
     string private offChainBaseURI;
-    
+
     /// @dev Byte size of each on-chain photo
-    uint256 immutable private photoDataByteSize = 5128;
+    uint256 private constant photoDataByteSizeMin = 5124;
+    uint256 private constant photoDataByteSizeMax = 5128;
 
     /// @dev Maps each photo to its on-chain storage address
     mapping(uint256 => address) private onChainStorage;
@@ -36,10 +36,7 @@ contract TheHydraDataStore is ITheHydraDataStore, Owned {
 
     /// @param _owner The owner of the contract, when deployed
     /// @param _offChainBaseURI The base url for any assets in this collection, i.e. an IPFS link
-    constructor(
-        address _owner,
-        string memory _offChainBaseURI
-    ) Owned(_owner) {
+    constructor(address _owner, string memory _offChainBaseURI) Owned(_owner) {
         offChainBaseURI = _offChainBaseURI;
     }
 
@@ -49,9 +46,7 @@ contract TheHydraDataStore is ITheHydraDataStore, Owned {
 
     /// @notice Admin function to set the baseURI for off-chain photos, i.e. an IPFS link
     /// @param _baseURI The new baseURI to set
-    function setOffChainBaseURI(
-        string memory _baseURI
-    ) external onlyOwner {
+    function setOffChainBaseURI(string memory _baseURI) external onlyOwner {
         offChainBaseURI = _baseURI;
     }
 
@@ -69,13 +64,15 @@ contract TheHydraDataStore is ITheHydraDataStore, Owned {
     /// @dev Uses SSTORE2 to store bytes data as a stand-alone contract
     /// @param _originalId The originalId of the photo, not the editionId. You can use the TheHydra.getOriginalId() to get the originalId of any edition
     /// @param _data The raw data in the .xqst format
-    function storeData(
-        uint256 _originalId,
-        bytes calldata _data
-    ) external onlyOwner {
-
+    function storeData(uint256 _originalId, bytes calldata _data)
+        external
+        onlyOwner
+    {
         /// @dev Currently storing 1 photo per storage contract -- this can be optimized to chunk more data into each storage contract!
-        if (_data.length != photoDataByteSize) revert InvalidMemorySequence();
+        if (
+            _data.length < photoDataByteSizeMin ||
+            _data.length > photoDataByteSizeMax
+        ) revert InvalidMemorySequence();
 
         onChainStorage[_originalId] = SSTORE2.write(_data);
     }
@@ -83,12 +80,7 @@ contract TheHydraDataStore is ITheHydraDataStore, Owned {
     /// @notice Gets the data in .xqst format
     /// @dev Our renderer contract will uses this when generating the metadata
     /// @param _originalId The originalId of the photo, not the editionId. You can use the TheHydra.getOriginalId() to get the originalId of any edition
-    function getData(
-        uint256 _originalId
-    ) external view returns (
-        bytes memory
-    )
-    {
+    function getData(uint256 _originalId) external view returns (bytes memory) {
         return SSTORE2.read(onChainStorage[_originalId]);
     }
 }
