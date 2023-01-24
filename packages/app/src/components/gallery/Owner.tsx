3390d3e3176200c6e98e23ffcd6ba6e8f9aad798
@therightchoyce <chris@therightchoyce.com>
2023-01-24 08:19:30 -0500

    Decrease text size for owner in the case of longer ENS names

--- packages/app/src/components/gallery/Owner.tsx
+++ packages/app/src/components/gallery/Owner.tsx
@@ -6,12 +6,12 @@ interface IOwner {
 
 export const Owner = ({ owner }: IOwner) => {
   return (
-    <div className="flex flex-row items-center content-center justify-center">
-      <div className="text-3xl font-extralight">Owner:</div>
+    <div className="flex flex-row items-center content-center justify-center h-12">
+      <div className="text-md lg:text-2xl font-extralight">Owner:</div>
       <div className="ml-4">
-        <div className="flex flex-row items-center text-3xl font-extralight">
-          <span className="rounded-full bg-rose-600 h-8 w-8 inline-block mr-2"></span>
-          <span className="underline underline-offset-8">
+        <div className="flex flex-row items-center text-md lg:text-2xl font-extralight">
+          <span className="rounded-full bg-rose-600 h-6 w-6 lg:h-8 lg:w-8 inline-block mr-2"></span>
+          <span className="border-b-2 border-slate-700 text-md lg:text-3xl font-extralight">
             <OwnerName address={owner || "Unknown"} />
           </span>
         </div>
